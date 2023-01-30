258afff0203e922bf4273bb55b496da50c3ffd32
pacyL2K19 <pacilinja2@gmail.com>
2023-01-30 08:38:07 +0200

    part-2(resolvers): test the rest of resolvers

--- src/app/auth/auth.resolver.spec.ts
+++ src/app/auth/auth.resolver.spec.ts
@@ -1,7 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
+import { Schema as MongooSchema } from 'mongoose';
+import * as Chance from 'chance';
+import { CreateUserInput } from '../user/dto/create-user.input';
 
+const userId = new MongooSchema.Types.ObjectId('');
+const chance = new Chance();
+
+const signupUserInput: CreateUserInput = {
+  address: chance.string({ length: 10 }),
+  email: chance.email(),
+  name: chance.string({ length: 12 }),
+  password: 'NewPassword2!',
+};
 describe('AuthResolver', () => {
   let resolver: AuthResolver;
 
@@ -11,7 +23,31 @@ describe('AuthResolver', () => {
         AuthResolver,
         {
           provide: AuthService,
-          useValue: {},
+          useValue: {
+            validateUser: jest.fn(() => {
+              // shpuld return a user
+              return {
+                _id: userId,
+                ...signupUserInput,
+              };
+            }),
+            login: jest.fn(() => {
+              return {
+                user: {
+                  _id: userId,
+                  ...signupUserInput,
+                },
+                authToken:
+                  'a-fake-token-as-we-tested-our-service-we-are-sure-we-get-it-from-the-service',
+              };
+            }),
+            signup: jest.fn(() => {
+              return {
+                _id: userId,
+                ...signupUserInput,
+              };
+            }),
+          },
         },
       ],
     }).compile();
@@ -19,7 +55,23 @@ describe('AuthResolver', () => {
     resolver = module.get<AuthResolver>(AuthResolver);
   });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
+  it('create a new user', async () => {
+    const user = await resolver.signup(signupUserInput);
+
+    expect(user.name).toBe(signupUserInput.name);
+    expect(user.email).toBe(signupUserInput.email);
+  });
+
+  it('should log in an existing user', async () => {
+    const user = resolver.login(
+      {
+        email: signupUserInput.email,
+        password: signupUserInput.password,
+      },
+      // the second argument is supposed to be a context, we are passing an empty object
+      {},
+    );
+
+    expect(user.user.email).toBe(signupUserInput.email);
   });
 });
--- src/app/book/book.resolver.spec.ts
+++ src/app/book/book.resolver.spec.ts
@@ -1,6 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookResolver } from './book.resolver';
 import { BookService } from './book.service';
+import { CreateBookInput } from './dto/create-book.input';
+import { UpdateBookInput } from './dto/update-book.input';
+import { Schema as MongooSchema } from 'mongoose';
+import * as Chance from 'chance';
+import { createAuthorInput } from '../author/author.service.spec';
+
+const DESCRIPTION_LENGTH = 20;
+
+const chance = new Chance();
+let bookId = new MongooSchema.Types.ObjectId('');
+
+const createBookInput: CreateBookInput = {
+  author: new MongooSchema.Types.ObjectId(''),
+  coverImage: chance.url(),
+  description: chance.string({ length: DESCRIPTION_LENGTH }),
+  isbn: chance.string(),
+  price: 12.09,
+  title: chance.string({ length: 10 }),
+};
+
+const updateBookInput: UpdateBookInput = {
+  _id: new MongooSchema.Types.ObjectId(''),
+  coverImage: chance.url(),
+  price: 20.99,
+};
 
 describe('BookResolver', () => {
   let resolver: BookResolver;
@@ -11,7 +36,51 @@ describe('BookResolver', () => {
         BookResolver,
         {
           provide: BookService,
-          useValue: {},
+          useValue: {
+            createBook: jest.fn(() => {
+              return {
+                _id: bookId,
+                ...createBookInput,
+              };
+            }),
+            findAllBooks: jest.fn(() => {
+              return {
+                books: [
+                  {
+                    ...createBookInput,
+                    author: {
+                      ...createAuthorInput,
+                      _id: createBookInput.author,
+                    },
+                    readers: [],
+                    _id: bookId,
+                  },
+                ],
+                booksCount: 1,
+              };
+            }),
+            getBookById: jest.fn(() => {
+              return {
+                ...createBookInput,
+                _id: bookId,
+                readers: [],
+                author: {
+                  _id: createBookInput.author,
+                  ...createAuthorInput,
+                },
+              };
+            }),
+            updateBook: jest.fn(() => {
+              return {
+                _id: bookId,
+                ...createBookInput,
+                ...updateBookInput,
+              };
+            }),
+            removeBook: jest.fn(() => {
+              return {};
+            }),
+          },
         },
       ],
     }).compile();
@@ -19,7 +88,63 @@ describe('BookResolver', () => {
     resolver = module.get<BookResolver>(BookResolver);
   });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
+  it('should be able to create a book', async () => {
+    const book = await resolver.createBook(createBookInput);
+
+    expect(book).toBeDefined();
+    expect(book.title).toBe(createBookInput.title);
+    expect(book.description.length).toBe(DESCRIPTION_LENGTH);
+
+    bookId = book._id;
+    updateBookInput._id = book._id;
+  });
+
+  it('should get a list of books, paginated', async () => {
+    const books = await resolver.findAllBooks({ limit: 10, skip: 0 });
+
+    expect(books.booksCount).toBe(1);
+    expect(books.books[0].title).toBe(createBookInput.title);
+    expect(books.books[0].author.name).toBe(createAuthorInput.name);
+    expect(books.books[0].readers.length).toBe(0);
+  });
+
+  it('should get a book by its id', async () => {
+    const book = await resolver.findOne({
+      limit: 10,
+      skip: 0,
+      _id: updateBookInput._id,
+    });
+
+    expect(book.title).toBe(createBookInput.title);
+    expect(book.price).toBe(createBookInput.price);
+    expect(book.author).toBeDefined();
+    expect(book.readers.length).toBeLessThanOrEqual(10); // 10 is the limit
+    expect(book.readers.length).toBe(0);
+  });
+
+  it('should be able to update a book', async () => {
+    const updatedBook = await resolver.updateBook(updateBookInput);
+
+    expect(updatedBook.coverImage).not.toBe(createBookInput.coverImage);
+    expect(updatedBook.coverImage).toBe(updateBookInput.coverImage);
+    expect(updatedBook.price).toBe(updateBookInput.price);
+
+    // the rest of the book details remain the same
+    expect(updatedBook.title).toBe(createBookInput.title);
+  });
+
+  it('should remove the book', async () => {
+    const removedBook = resolver.removeBook(bookId);
+    expect(removedBook).toBeDefined();
+  });
+
+  it('should throw an error when getting a removed user', async () => {
+    try {
+      await resolver.findOne({ skip: 0, limit: 10, _id: bookId });
+    } catch (error) {
+      expect(error).toBeDefined();
+      expect(error.response).toBeDefined();
+      expect(error.response.statusCode).toBe(404);
+    }
   });
 });
