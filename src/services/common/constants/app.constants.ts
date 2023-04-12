654e2e88970fad61deee9764c4df3438ab30c250
pacyL2K19 <pacilinja2@gmail.com>
2023-04-12 09:25:05 +0200

    part-4 (chore): setup and install packages for the api-gateway service

---
+++ src/services/common/constants/app.constants.ts
@@ -0,0 +1,3 @@
+export const INVALID_AUTH_TOKEN =
+  'Invalid auth token, make sure you are authenticated';
+export const INVALID_BEARER_TOKEN = 'Invalid token, please log in again';
---
+++ src/services/common/exceptions/user.exceptions.ts
@@ -0,0 +1,5 @@
+export const USER_ALREADY_EXIST_EXCEPTION =
+  'User already exists, login instead';
+
+export const CANNOT_PURCHASE_MORE_THAN_ONE_COPY_EXCEPTION =
+  'You already have a copy of this book';
